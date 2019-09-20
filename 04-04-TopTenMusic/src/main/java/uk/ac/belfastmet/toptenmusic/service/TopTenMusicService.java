2a46c913ee8dc6a7c541a2561c3f9f89509e968b
cre19171872 <cre19171872@users.noreply.github.com>
2019-09-20 12:46:28 +0100

    end of class

--- 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/toptenmusic/service/TopTenMusicService.java
+++ 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/toptenmusic/service/TopTenMusicService.java
@@ -2,8 +2,11 @@ package uk.ac.belfastmet.toptenmusic.service;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import uk.ac.belfastmet.toptenmusic.controller.TopTenMusicController;
 import uk.ac.belfastmet.toptenmusic.domain.TopTenMusic;
 
 @Service
@@ -11,6 +14,8 @@ public class TopTenMusicService {
 
 	private ArrayList<TopTenMusic> topTenAlbums;
 	private ArrayList<TopTenMusic> topTenSingles;
+	
+	Logger logger = LoggerFactory.getLogger(TopTenMusicService.class);
 
 	public ArrayList<TopTenMusic> getTopTenAlbums() {
 		this.topTenAlbums = new ArrayList<TopTenMusic>();
@@ -19,7 +24,7 @@ public class TopTenMusicService {
 		this.topTenAlbums.add(new TopTenMusic("On the Moon-Beam", "Vaughn Monroe", "cover2.jpg"));
 		this.topTenAlbums.add(new TopTenMusic("King Cole Trio", "King Cole Trio", "cover3.jpg"));
 		
-
+		logger.info("Album service loaded");
 		return topTenAlbums;
 	}
 
@@ -30,6 +35,7 @@ public class TopTenMusicService {
 		this.topTenSingles.add(new TopTenMusic("Rum and Coca-Cola", "Andrews Sisters", "sing2.jpg"));
 		this.topTenSingles.add(new TopTenMusic("Till the End of Time", "Perry Como", "sing3.jpg"));
 		
+		logger.info("Single service loaded");
 		return topTenSingles;
 
 	}
