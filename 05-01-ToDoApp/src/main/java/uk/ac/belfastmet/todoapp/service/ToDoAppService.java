1216d0a0888fb60fb3925d96408f6da52024139b
cre19171872 <cre19171872@users.noreply.github.com>
2019-09-23 13:47:23 +0100

    add thymleaf conditionals

--- 05-01-ToDoApp/src/main/java/uk/ac/belfastmet/todoapp/service/ToDoAppService.java
+++ 05-01-ToDoApp/src/main/java/uk/ac/belfastmet/todoapp/service/ToDoAppService.java
@@ -17,9 +17,9 @@ public class ToDoAppService {
 		this.taskList = new ArrayList<ToDo>();
 		
 		this.taskList.add(new ToDo("23-09-2019","Deliver award winning presentation", "TedTalk", false, 3,"Colin"));
-		this.taskList.add(new ToDo("24-09-2019","Learn how to handstand", "Gymnastics", false, 2,"Albus"));
+		this.taskList.add(new ToDo("24-09-2019","Learn how to handstand", "Gymnastics", true, 2,"Albus"));
 		this.taskList.add(new ToDo("25-09-2019","Write million pound app", "Code", false, 1,"Kvoth"));
-		this.taskList.add(new ToDo("26-09-2019","Wash the dishes", "Chores", false, 1,"Geralt"));
+		this.taskList.add(new ToDo("26-09-2019","Wash the dishes", "Chores", true, 3,"Geralt"));
 		this.taskList.add(new ToDo("27-09-2019","Vaccuum the cat", "Pets", false, 1,"Pugwash"));
 		
 		logger.info("Tasks loaded");
